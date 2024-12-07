be3758c82516a13cff7fb7dc73c46e6b07fbdaf8
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2024-12-07 15:43:00 +0100

    first two tests

---
+++ src/test/java/fesle/playwright/ASimplePlaywrightTest.java
@@ -0,0 +1,38 @@
+package fesle.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ASimplePlaywrightTest {
+
+    @Test
+    void shouldShowThePageTitle(){
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void shouldSearchByKeyword() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("[placeholder=Search]").fill("dddd");
+        page.locator("button:has-text('Search')").click();
+        int matchingSearchResults = page.locator(".card").count();
+
+        Assertions.assertTrue(matchingSearchResults > 0);
+        browser.close();
+        playwright.close();
+    }
+}
