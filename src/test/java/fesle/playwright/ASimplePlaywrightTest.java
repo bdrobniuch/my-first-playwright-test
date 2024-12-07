39960bb47ba1664ab5b6f468472434b978091271
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2024-12-07 16:35:50 +0100

    Before After

--- src/test/java/fesle/playwright/ASimplePlaywrightTest.java
+++ src/test/java/fesle/playwright/ASimplePlaywrightTest.java
@@ -4,36 +4,50 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
 
-    @Test
-    void shouldShowThePageTitle(){
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-        page.navigate("https://practicesoftwaretesting.com/");
-        String title = page.title();
-        Assertions.assertTrue(title.contains("Practice Software Testing"));
+    Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    void setup ()
+    {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void teardown(){
         browser.close();
         playwright.close();
     }
 
     @Test
+    void shouldShowThePageTitle(){
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+
+    }
+
+    @Test
     void shouldSearchByKeyword() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+
         page.navigate("https://practicesoftwaretesting.com/");
         page.locator("[placeholder=Search]").fill("dddd");
         page.locator("button:has-text('Search')").click();
         int matchingSearchResults = page.locator(".card").count();
 
         Assertions.assertTrue(matchingSearchResults > 0);
-        browser.close();
-        playwright.close();
+
 
     }
 }
