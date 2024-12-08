8ab4e6c8830b80be2c70763321c55e08fd9c8106
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2024-12-08 11:29:55 +0100

    najnowsze zmiany

--- src/test/java/fesle/playwright/ASimplePlaywrightTest.java
+++ src/test/java/fesle/playwright/ASimplePlaywrightTest.java
@@ -1,13 +1,36 @@
 package fesle.playwright;
 
 
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.options.LoadState;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@UsePlaywright
+import java.util.Arrays;
+
+@UsePlaywright(ASimplePlaywrightTest.CustomOptions.class)
 public class ASimplePlaywrightTest {
+
+    public static class CustomOptions implements OptionsFactory {
+        @Override
+        public Options getOptions() {
+            return new Options()
+
+                    .setHeadless(false)
+
+                    .setLaunchOptions(
+                            new BrowserType.LaunchOptions()
+                                    .setSlowMo(1000)
+                                    .setArgs(Arrays.asList("--no-sandbox",
+                                            "--disable-gpu",
+                                            "--disable-extensions"))
+                    );
+        }
+    }
 
 
     @Test
@@ -24,10 +47,10 @@ public class ASimplePlaywrightTest {
     void shouldSearchByKeyword(Page page) {
 
         page.navigate("https://practicesoftwaretesting.com/");
-        page.locator("[placeholder=Search]").fill("dddd");
+        page.locator("[placeholder=Search]").fill("Pliers");
         page.locator("button:has-text('Search')").click();
         int matchingSearchResults = page.locator(".card").count();
-
+        page.waitForLoadState(LoadState.NETWORKIDLE);
         Assertions.assertTrue(matchingSearchResults > 0);
 
 
