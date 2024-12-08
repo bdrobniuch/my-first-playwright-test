cf6bd21ebd4dc68c3168c8153dd07f879626f7f1
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2024-12-08 14:37:21 +0100

    corrected comment

--- src/test/java/fesle/playwright/ASimplePlaywrightTest.java
+++ src/test/java/fesle/playwright/ASimplePlaywrightTest.java
@@ -53,10 +53,7 @@ public class ASimplePlaywrightTest {
                                             "--disable-gpu",
                                             "--disable-extensions"))
                     );
-        }
-    }
     */
-
     @Test
     void shouldShowThePageTitle(){
 
