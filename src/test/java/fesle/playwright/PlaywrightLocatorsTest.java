c7640d80a3bf0d092c48d068def8767eccc08bb0
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2024-12-08 14:08:04 +0100

    PlaywrightLocatorsTest Locating by label

--- src/test/java/fesle/playwright/PlaywrightLocatorsTest.java
+++ src/test/java/fesle/playwright/PlaywrightLocatorsTest.java
@@ -31,7 +31,7 @@ public class PlaywrightLocatorsTest {
         }
 
         @AfterEach
-        public void tearDown(){
+         void tearDown(){
             browser.close();
             playwright.close();
         }
@@ -42,5 +42,53 @@ public class PlaywrightLocatorsTest {
             page.getByText("Bolt Cutters");
             PlaywrightAssertions.assertThat(page.getByText("MightyCraft Hardware")).isVisible();
         }
+
+        @DisplayName("Locating an element by alternative text")
+        @Test
+        void byAltTexg()
+        {
+            page.getByAltText("Combination Pliers");
+            PlaywrightAssertions.assertThat(page.getByText("ForgeFlex Tools")).isVisible();
+        }
+    }
+
+    @DisplayName("Locating elements by label")
+    @Nested
+    class LocateElementsByLabel {
+        private Playwright playwright;
+        private Browser browser;
+        private Page page;
+
+        @BeforeEach
+        void openCatalogPage(){
+             playwright = Playwright.create();
+             browser = playwright.chromium().launch(
+                     new BrowserType.LaunchOptions()
+                             .setHeadless(false)
+                             .setSlowMo(2000)
+                             .setArgs(Arrays.asList("--no-sandbox","--disable-extensions","--disable-gpu"))
+             );
+
+             page =browser.newPage();
+             page.navigate("https://practicesoftwaretesting.com/");
+
+        }
+
+        @AfterEach
+        void tearDown ()
+        {
+            browser.close();
+            playwright.close();
+        }
+
+        @DisplayName("Fill out form by label")
+        @Test
+        void fillOutForm() {
+            page.locator("a:text('Contact')").click();
+            page.getByLabel("First Name").fill("Obi");
+            page.getByLabel("Last Name").fill("Kenobi");
+            page.getByLabel("Email address").fill("deathstar@gmail.com");
+        }
+
     }
 }
