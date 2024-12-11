30a9915a3bbc233dece0c721082c4f86fbb2821f
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2024-12-11 15:12:22 +0100

    Playwright Waits 2

--- src/test/java/fesle/playwright/PlaywrightWaitTests.java
+++ src/test/java/fesle/playwright/PlaywrightWaitTests.java
@@ -1,9 +1,12 @@
 package fesle.playwright;
 
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.junit.UsePlaywright;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,24 +14,46 @@ import java.util.List;
 @UsePlaywright(ChromeOptions.class)
 public class PlaywrightWaitTests {
 
-    @BeforeEach
-    void openContactPage(Page page) {
-        page.navigate("https://practicesoftwaretesting.com");
-        page.waitForSelector("[data-test=product-name]");
+    @Nested
+    class WaitingForState {
+
+        @BeforeEach
+        void openContactPage(Page page) {
+            page.navigate("https://practicesoftwaretesting.com");
+            page.waitForSelector("[data-test=product-name]");
+        }
+
+        @Test
+        void shouldShowAllProductNames(Page page){
+            List<String> productNames = page.getByTestId("product-name").allInnerTexts();
+            Assertions.assertThat(productNames).contains("Pliers", "Bolt Cutters", "Hammer");
+        }
+
+        @Test
+        void shouldShowAllProductImages(Page page) {
+            List<String> productImagesTitles = page.locator(".card-img-top").all()
+                            .stream()
+                            .map(img -> img.getAttribute("alt"))
+                                    .toList();
+            Assertions.assertThat(productImagesTitles).contains("Pliers", "Bolt Cutters", "Hammer");
+        }
     }
 
-    @Test
-    void shouldShowAllProductNames(Page page){
-        List<String> productNames = page.getByTestId("product-name").allInnerTexts();
-        Assertions.assertThat(productNames).contains("Pliers", "Bolt Cutters", "Hammer");
+    @Nested
+    class AutomaticWaiting {
+
+        @BeforeEach
+        void openContactPage(Page page) {
+            page.navigate("https://practicesoftwaretesting.com");
+        }
+
+        @DisplayName( "Shoudl wait for the filter checkbox option to appear before clicking")
+        @Test
+        void shouldVaidForTheFilterCheckboxes (Page page) {
+            var screwDriverFilter = page.getByLabel("Screwdriver");
+            screwDriverFilter.click();
+            PlaywrightAssertions.assertThat(screwDriverFilter).isChecked();
+        }
     }
 
-    @Test
-    void shouldShowAllProductImages(Page page) {
-        List<String> productImagesTitles = page.locator(".card-img-top").all()
-                        .stream()
-                        .map(img -> img.getAttribute("alt"))
-                                .toList();
-        Assertions.assertThat(productImagesTitles).contains("Pliers", "Bolt Cutters", "Hammer");
-    }
 }
