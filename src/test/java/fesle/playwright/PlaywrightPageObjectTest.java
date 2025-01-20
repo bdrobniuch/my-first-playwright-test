f1c443137363045b7cd3bb8c54075b1d1c0be901
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-01-20 14:46:33 +0100

    simple PageObjects page

--- src/test/java/fesle/playwright/PlaywrightPageObjectTest.java
+++ src/test/java/fesle/playwright/PlaywrightPageObjectTest.java
@@ -67,5 +67,47 @@ public class PlaywrightPageObjectTest {
                     .contains("Tape Measure 7.5m", "Measuring Tape", "Tape Measure 5m");
 
         }
+
+
+        @DisplayName("With Page Objects")
+        @Test
+        void withPageObjects() {
+            SearchComponent searchComponent = new SearchComponent(page);
+            ProductList productList = new ProductList(page);
+
+            searchComponent.searchBy("tape");
+
+            var matchingProducts = productList.getProductNames();
+
+           Assertions.assertThat(matchingProducts)
+                    .contains("Tape Measure 7.5m", "Measuring Tape", "Tape Measure 5m");
+        }
+
+        class SearchComponent {
+            private final Page page;
+
+            SearchComponent(Page page) {
+                this.page = page;
+            }
+
+            public void searchBy(String tape) {
+                page.waitForResponse("**/products/search?q=tape", () -> {
+                    page.getByPlaceholder("Search").fill("tape");
+                    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
+                });
+            }
+        }
+
+        class ProductList {
+            private final Page page;
+
+            ProductList(Page page) {
+                this.page = page;
+            }
+
+            public List<String>  getProductNames() {
+                return page.getByTestId("product-name").allInnerTexts();
+            }
+        }
     }
 }
