d8de1898e7f1f4570cf3f76ceb5806ca8f7797f7
BDrobniuch <43449396+bdrobniuch@users.noreply.github.com>
2025-01-20 09:10:36 +0100

    PlaywrightTestCase extends

---
+++ src/test/java/fesle/playwright/login/LoginWithRegisteredUserTest.java
@@ -0,0 +1,13 @@
+package fesle.playwright.login;
+
+import fesle.playwright.PlaywrightTestCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LoginWithRegisteredUserTest extends PlaywrightTestCase {
+    @Test
+    @DisplayName("Should be able to login with a registered user")
+    void should_login_registered_user() {
+
+    }
+}
